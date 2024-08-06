25f7e3aead2504f157cbcb746e13e7061de99a08
y-hirakaw <130817538+y-hirakaw@users.noreply.github.com>
2024-08-06 22:26:45 +0900

    追加画面を追加

--- PearentEmotionApp/iosApp/iosApp/EmotionList/EmotionListView.swift
+++ PearentEmotionApp/iosApp/iosApp/EmotionList/EmotionListView.swift
@@ -1,16 +1,70 @@
 import SwiftUI
 
+struct Emotion: Identifiable {
+    let id = UUID()
+    let name: String
+}
+
+class EmotionStore: ObservableObject {
+    @Published var emotions: [Emotion] = []
+
+    func addEmotion(name: String) {
+        let newItem = Emotion(name: name)
+        emotions.append(newItem)
+    }
+}
+
 struct EmotionListView: View {
-    @State private var items = ["アイテム1", "アイテム2", "アイテム3"]
+    @StateObject var emotionStore = EmotionStore()
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items, id: \.self) { item in
-                    Text(item)
+            VStack {
+                List(emotionStore.emotions) { item in
+                    Text(item.name)
+                }
+
+                NavigationLink(destination: AddEmotionView(emotionStore: emotionStore)) {
+                    Text("Add Emotion")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .padding()
                 }
             }
+            .navigationTitle("Emotions")
         }
+    }
+}
+
+struct AddEmotionView: View {
+    @ObservedObject var emotionStore: EmotionStore
+    @Environment(\.presentationMode) var presentationMode
+    @State private var emotionName: String = ""
+
+    var body: some View {
+        VStack {
+            TextField("Emotion Name", text: $emotionName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Button(action: {
+                emotionStore.addEmotion(name: emotionName)
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Save")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    .padding()
+            }
+        }
+        .navigationTitle("Add Emotion")
+        .padding()
     }
 }
 
